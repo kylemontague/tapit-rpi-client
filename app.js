7a49b15421afe8006d5bc6f963e9c4ba8f6dec51
Kyle Montague <kyle.montague@ncl.ac.uk>
2019-04-06 16:20:43 +0100

    emitting some crimes take 66

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const flow = require('./utils/flow-meter')
+const googlehome = require('./utils/google-home')
+
 
 const TAP_1_CHANNEL = 3
 const TAP_2_CHANNEL = 4
@@ -11,6 +13,7 @@ function initTaps(){
     flow.init(TAP_CHANNELS)
     flow.emitter.on("served",(data) =>{
         console.log(`tap:${data.tap}, volume:${data.volume}`)
+        googlehome.speak(`Kyle just poured ${Math.round(data.volume)}ml from tap ${data.tap}. Fuck Yeah!`)
     })
 
     flow.emitter.on("serving",(data) =>{
