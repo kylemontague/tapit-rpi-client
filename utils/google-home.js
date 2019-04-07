8d0c69aa1cc80386b21595a62476ff3c3c9c5758
Kyle Montague <kyle.montague@ncl.ac.uk>
2019-04-07 19:14:53 +0100

    +removed all of the silly stuff Hugo wanted.

--- utils/google-home.js
+++ utils/google-home.js
@@ -1,12 +1,16 @@
-const GoogleHome = require('node-googlehome')
+let Request = require('rest-request');
+var restAPI = new Request('http://192.168.2.194:8008');
+
+var headers = { "Content-Type": "application/json" }
 
 
-let device = new GoogleHome.Connecter('192.168.2.194')
+restAPI.get("setup/eureka_info","audio",headers)
+.then(data =>{
+    console.log(data)
+}).catch(err =>{
+    console.error(err)
+})
 
-device.config({lang:'en'})
-
-device.readySpeaker()
-    .then(()=>{})
 
 function speak(text){
     device.speak(text).then(console.log(text))
--- utils/tapit-web-client.js
+++ utils/tapit-web-client.js
@@ -1,3 +1,24 @@
 let Request = require('rest-request');
-var restAPI = new Request('');
+let restAPI = new Request('URL');
 
+let API = ""
+
+function setAPIKey(key){
+    API = key
+}
+
+function getHeaders(){
+    return {api_key:API}
+}
+
+function places(){
+    return restAPI.get("places",{},getHeaders())
+}
+
+function tapsAtPlace(place){
+    return restAPI.get("places/:id/taps",{id:place},getHeaders())
+}
+
+function addServing(tap,volume){
+    return restAPI.post("serving/create/")
+}
